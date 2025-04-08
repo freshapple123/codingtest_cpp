fcdeb5bf1456571d585b911573ac9909f47505ac
jiwoo <jju0531@naver.com>
2025-04-08 19:53:39 +0900

    04.08

--- 정지우 연습장/main/coding_test/test.cpp
+++ 정지우 연습장/main/coding_test/test.cpp
@@ -1,12 +1,22 @@
 #include<iostream>
-#include<algorithm>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
-struct Point
+int main()
 {
-	int x, y;
-	
-	Point(int x, int y) : x(x), y(y) {}
-};
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	vector<int> v = { 1,2,2,3,3,3,4,4,5,5,5 };
+
+	auto newEnd = unique(v.begin(), v.end());
+
+	for (auto it = v.begin(); it != newEnd; it++)
+	{
+		cout << *it << " ";
+	}
+
+	return 0;
+}
