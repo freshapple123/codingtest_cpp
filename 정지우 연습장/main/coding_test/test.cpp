98af40006c78972a16c33dd0bfc56f70b0007a02
jiwoo <jju0531@naver.com>
2025-05-01 20:39:10 +0900

    ad

--- 정지우 연습장/main/coding_test/test.cpp
+++ 정지우 연습장/main/coding_test/test.cpp
@@ -1,22 +1,51 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-
+#include<string>
 using namespace std;
 
-int main()
+bool visited[11][11][4];
+
+int dx[] = { 0,1,0,-1 };
+int dy[] = { -1,0,1,0 };
+
+int todir(char dir)
 {
-	cin.tie(nullptr);
-	ios_base::sync_with_stdio(false);
+	int ret;
+	if (dir == 'U')
+		ret = 0;
+	else if (dir == 'R')
+		ret = 1;
+	else if (dir == 'D')
+		ret = 2;
+	else
+		ret = 3;
+	return ret;
+}
 
-	vector<int> v = { 1,2,2,3,3,3,4,4,5,5,5 };
+bool isNotValid(int x, int y) { return x < 0 || y < 0 || x>10 || y>10; }
 
-	auto newEnd = unique(v.begin(), v.end());
+int opposite_direction(int dir) { return (dir + 2) % 4; }
 
-	for (auto it = v.begin(); it != newEnd; it++)
+int solution(string dirs)
+{
+	int answer = 0;
+	int x = 5, y = 5;
+
+	for (auto c : dirs)
 	{
-		cout << *it << " ";
-	}
+		int dir = todir(c);
+		int nx = x + dx[dir];
+		int ny = y + dy[dir];
 
-	return 0;
+		if (isNotValid(nx, ny))
+			continue;
+
+		if (visited[y][x][dir] == false)
+		{
+			visited[y][x][dir] = true;
+			visited[ny][nx][opposite_direction(dir)] = true;
+			answer++;
+		}
+		x = nx;
+		y = ny;
+	}
+	return answer;
 }
